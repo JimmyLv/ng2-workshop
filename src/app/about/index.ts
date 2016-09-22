8a7d906367ff1de718875e15778b65277c269a27
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-09-22 19:07:30 +0800

    [#3] - Moved app-bar into shared components and add route logic for about and home page.

---
+++ src/app/about/index.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'about',
+    template: require('./about.html'),
+    styles: [require('./about.css')],
+})
+export class About {
+
+}
