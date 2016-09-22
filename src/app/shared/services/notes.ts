ec9af0ee6cd0710a0ed4f1d17ae587740128a49b
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-09-22 18:00:54 +0800

    [#3] - Simplify noteService constructor.

--- src/app/shared/services/notes.ts
+++ src/app/shared/services/notes.ts
@@ -3,11 +3,9 @@ import { ApiService } from './api'
 
 @Injectable()
 export class NoteService {
-  private apiService: ApiService
   path: string = '/notes'
 
-  constructor(apiService: ApiService) {
-    this.apiService = apiService
+  constructor(private apiService: ApiService) {
   }
 
   createNote(note) {
