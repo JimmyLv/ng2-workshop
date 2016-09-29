555ab467d333650ed679de81a7ae59b3c87d126c
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-09-29 18:53:23 +0800

    [#4] - Added auth component related content and event handlers.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core'
 
+const linkText = {
+  signIn: `Don't have an account?`,
+  signUp: `Already have an account?`
+}
+
 @Component({
-  selector: 'auth',
-  template: require('./auth.component.html')
+  template: require('./auth.component.html'),
+  styles: [require('./auth.component.css')]
 })
 export class AuthComponent implements OnInit {
+  user: { email: string, password: string }
+  mode: 'signIn' | 'signUp'
+  linkText: string
+
   constructor() {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.user = { email: '', password: '' }
+    this.mode = 'signIn'
+    this.linkText = linkText.signIn
   }
 
+  changeMode(): void {
+    if (this.mode === 'signIn') {
+      this.mode = 'signUp'
+      this.linkText = linkText.signUp
+    } else {
+      this.mode = 'signIn'
+      this.linkText = linkText.signIn
+    }
+  }
 }
