6b1f2102909adc0f5830f7651a814ca7dd3509fd
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-09-29 17:50:30 +0800

    [#4] - Added main component and auth service them use it in route.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,10 +2,19 @@ import { RouterConfig } from '@angular/router'
 import { Home } from './home/'
 import { About } from './about/'
 import { AuthComponent } from './auth/auth.component'
+import { MainComponent } from './main/main.component'
+import { AuthService } from './shared/services/auth.service'
 
 export const router: RouterConfig = [
-  { path: '', component: Home },
-  { path: 'about', component: About },
+  {
+    path: '',
+    component: MainComponent,
+    canActivate: [AuthService],
+    children: [
+      { path: '', component: Home },
+      { path: 'about', component: About },
+    ]
+  },
   { path: 'auth', component: AuthComponent },
   { path: '**', redirectTo: '' },
 ]
